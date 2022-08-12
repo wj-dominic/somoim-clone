45ebc85e9771a74253d8c48211a507cc077bbe21
wj-dominic <ywj8909@gmail.com>
2022-08-12 23:26:28 +0900

    jpa 설정을 별도 빈으로 만들어 사용하도록 변경

--- src/main/java/com/somoim/config/JpaConfig.java
+++ src/main/java/com/somoim/config/JpaConfig.java
@@ -2,7 +2,10 @@ package com.somoim.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,22 +13,23 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
-@EnableJpaRepositories(basePackages="com.somoim.repository")
+@EnableJpaRepositories(basePackages = "com.somoim.repository")
 public class JpaConfig {
+
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource lazyRoutingDataSource) {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.show_sql", true);
-        properties.put("hibernate.format_sql", true);
-        properties.put("hibernate.hbm2ddl.auto", "create");
+    public JpaProperties jpaProperties() {
+        return new JpaProperties();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource lazyRoutingDataSource, JpaProperties jpaProperties) {
+        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 
         final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(lazyRoutingDataSource);
         entityManagerFactoryBean.setPackagesToScan("com.somoim.model.entity");
-        entityManagerFactoryBean.setJpaPropertyMap(properties);
-
-
-        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactoryBean.setJpaPropertyMap(jpaProperties.getProperties());
         entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
 
         return entityManagerFactoryBean;
